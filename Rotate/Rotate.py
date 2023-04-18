eb95b5829c4b1d717f6e09a37bb43e09c9cc6460
BurNing <766489261@qq.com>
2023-04-18 18:09:54 +0800

    增加翻转逻辑

--- Rotate/Rotate.py
+++ Rotate/Rotate.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 import math
 import sys
 import os
@@ -9,21 +9,48 @@ if debug:
     pathRoot = 'Rotate\\Images\\origin'
     outRoot = 'Rotate\\Images\\Rotate'
     angle = -50
+    type = 2
 else:
     pathRoot = sys.argv[1]
     angle = int(sys.argv[2])
+    type = int(sys.argv[3])
     outRoot = pathRoot
     print("Release模式\nreadPath: ", pathRoot, " angle: ", angle)
 
 filePaths = os.listdir(pathRoot)
 
-def rotateImages():
+def transpose(img_path, out_path):
+    # 打开PNG图片
+    image = Image.open(img_path)
+
+    # 水平翻转
+    if type == 1:
+        img_out = image.transpose(method=Image.FLIP_LEFT_RIGHT)
+
+    # 垂直翻转
+    if type == 2:
+        img_out = image.transpose(method=Image.FLIP_TOP_BOTTOM)
+
+    dir = os.path.dirname(out_path)
+    exist = os.path.exists(dir)
+
+    if not exist:
+        os.makedirs(dir)
+
+    # 将旋转后的图片保存到本地文件
+    img_out.save(out_path)
+
+def manageImages():
     for i in range(0, len(filePaths)):
         img_path = str(pathRoot + "\\" + filePaths[i])
         out_path = str(outRoot + "\\" + filePaths[i])
 
-        rotation(img_path, out_path)
-        print("图片旋转: ", img_path, " angle: ", angle)
+        if type == 0:
+            rotation(img_path, out_path)
+            print("图片旋转: ", img_path, " angle: ", angle)
+        else:
+            transpose(img_path, out_path)
+            print("图片翻转: ", img_path, " angle: ", angle)
 
 def rotation(img_path, out_path):
     # 打开PNG图片
@@ -31,7 +58,6 @@ def rotation(img_path, out_path):
     
     # 旋转图片
     rotated_image = image.rotate(angle, resample=Image.BICUBIC, expand=True)
-
     dir = os.path.dirname(out_path)
     exist = os.path.exists(dir)
 
@@ -41,4 +67,4 @@ def rotation(img_path, out_path):
     # 将旋转后的图片保存到本地文件
     rotated_image.save(out_path)
 
-rotateImages()
+manageImages()
