b5df4550fd83f6dfff70511308f0d168e0fa24bc
BurNing <766489261@qq.com>
2023-05-11 11:32:34 +0800

    测试PyQt

---
+++ QT/Main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from UIProfeab import Ui_main
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_main.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
