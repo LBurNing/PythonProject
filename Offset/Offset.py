af1d50e977262862c5ea4b024836b54a8fa16651
BurNing <766489261@qq.com>
2025-04-10 15:14:45 +0800

    Unity0,0点在左下角, python中图片操作是在左上角 所以偏移的时候加上坐标系的偏差

--- Offset/Offset.py
+++ Offset/Offset.py
@@ -7,17 +7,11 @@ import concurrent.futures
 debug = sys.gettrace()
 if debug:
     print("Debug模式\n")
-    pathRoot = 'D:\\RXCQ\\源资源\\createRole\\'
-    outRoot = 'D:\\RXCQ\\源资源\\createRole\\'
-    scale = 0.8
-    offset_x = 0
-    offset_y = 0
+    pathRoot = r'D:\UnityProject\Card\Assets\Res\Textures\Animation\1004\\'
+    outRoot = r'D:\UnityProject\Card\Assets\Res\Textures\Animation\1004\\'
 else:
     pathRoot = sys.argv[1]
     outRoot = sys.argv[2]
-    scale = float(sys.argv[3])
-    offset_x = int(sys.argv[4])
-    offset_y = int(sys.argv[5])
     print("Release模式\nreadPath: ", pathRoot, " outPath: ", outRoot)
 
 filePaths = []
@@ -44,39 +38,31 @@ def offsetImage(img_path, out_path):
     y_offset = offsets[1]
 
     # 计算偏移后的新坐标
-    new_x = abs(x_offset) + width * 2 + 40
-    new_y = abs(y_offset) + height * 2 + 40
+    new_x = abs(x_offset) + width * 2 + 200
+    new_y = abs(y_offset) + height * 2 + 200
     
     # 创建一个新的空白图片，尺寸为偏移后的坐标
     new_image = Image.new("RGBA", (new_x, new_y), (0, 0, 0, 0))
 
+    # Unity0,0点在左下角, python中图片操作是在左上角
     # 将原始图片粘贴到新图片的偏移位置
-    new_image.paste(image, (new_x // 2 + x_offset, new_y // 2 + y_offset))  
+    new_image.paste(image, (new_x // 2 - width // 2 + x_offset, new_y // 2 - height // 2 - y_offset))
     
     # 保存处理后的图片
     new_image.save(out_path)
     return out_path
 
 def offsetImages():
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
         for i in range(0, len(filePaths)):
             img_path = filePaths[i]
             out_path = outRoot + "\\" + os.path.basename(img_path)
-            futures.append(executor.submit(offsetImage, img_path, out_path))
-
             dir = os.path.dirname(out_path)
             exist = os.path.exists(dir)
 
             if not exist:
                 os.makedirs(dir)
-
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                future.result()
-            except Exception as e:
-                print(e)
             
-            print("图片偏移完成: ", future.result())
+            offsetImage(img_path, out_path)
+            print("图片偏移完成: ", out_path)
 
 offsetImages()
